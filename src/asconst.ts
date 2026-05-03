922b3965b2022f5dde082781addbdcd29ff14b82
Ahmed-Nakib <office.nakib@gmail.com>
2026-05-03 22:04:33 +0600

    commit-2-9

---
+++ src/asconst.ts
@@ -0,0 +1,47 @@
+
+
+// const UserRoles = {
+//     Admin : "Admin",
+//     Editor : "Editor",
+//     Viewer : "Viewer"
+// } as const;
+
+// // typeof operator
+// // keyof operator
+
+// const canEdit = (role: keyof typeof UserRoles) => {
+//     if(role === UserRoles.Admin || role === UserRoles.Editor){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// const isEditPermissible = canEdit(UserRoles.Admin);
+// console.log(isEditPermissible);
+
+
+
+
+const UserRoles = {
+    Admin : "ADMIN",
+    Editor : "EDITOR",
+    Viewer : "VIEWER"
+} as const;
+
+
+const canEdit = (role: (typeof UserRoles)[keyof typeof UserRoles]) => {
+    if(role === UserRoles.Admin || role === UserRoles.Editor){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const isEditPermissible = canEdit(UserRoles.Admin);
+console.log(isEditPermissible);
+
+
+
