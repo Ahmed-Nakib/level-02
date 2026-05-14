284796625dc75a5832c395233b3cfa9f4deda9b6
Ahmed-Nakib <office.nakib@gmail.com>
2026-05-14 20:17:05 +0600

    misson-2-the-anatomy-of-node.js

--- src/utils.ts
+++ src/utils.ts
@@ -1,38 +1,47 @@
-import type { Req, Res } from "./type";
+import type { Req } from "./types";
+import type { Res } from "./types";
 
-export function sendResponse <T>(res: Res, {message, data , error}: {message: string, data?:T, error?: boolean }, status = 200) {
-    res.writeHead(status, {"content-type" : "application/json"})
-    res.end(JSON.stringify({
-        success: error ? false : true,
-        message: message,
-        data: error ? false : data
-    }))
+// @challenge: make this function that can handle text, html etc
+export function sendResponse<T>(
+  res: Res,
+  { message, data, error }: { message: unknown; data?: T; error?: boolean },
+  status = 200,
+): void {
+  res.writeHead(status, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      success: error ? false : true,
+      message: message,
+      data: error ? [] : data,
+    }),
+  );
 }
 
-export const extractRequestInfo = async <T> (req: Req) => {
-    const params = req.url?.split("/").filter(Boolean) ?? [];
-    const body = req.method === "POST" || req.method === "PUT" || req.method === "PATCH" ? await parseBody<T>(req) : null
-    return {
-        url: req.url ?? "/",
-        method: req.method,
-        params: params,
-        body: body
-    }
-}
+export const extractRequestInfo = async <T>(req: Req) => {
+  const body: T | null =
+    req.method === "POST" || req.method === "PUT" ? await parseBody<T>(req) : null;
+  const params = req.url?.split("/").filter(Boolean) ?? [];
+  return {
+    method: req.method ?? undefined,
+    url: req.url ?? "/",
+    body,
+    params,
+  };
+};
 
-
-const parseBody = async <T> (req: Req): Promise<T | null> => {
-    return new Promise((resolve, reject) => {
-        let body = "";
-        req.on("data", (chunk) => {
-            body += chunk.toString()
-        })
-        req.on("end", () => {
-            try {
-                resolve(JSON.parse(body))
-            } catch (error) {
-                reject(new Error("invalid data"))
-            }
-        })
-    })
-}
+export const parseBody = async <T>(req: Req): Promise<T | null> => {
+  return new Promise((resolve, reject) => {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      try {
+        resolve(JSON.parse(body));
+      } catch (error) {
+        reject(new Error("Invalid JSON"));
+      }
+    });
+    req.on("error", reject);
+  });
+};
